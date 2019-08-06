dfdbfa5dafcee0c45c046306df4e07a8df4e5358
roxanaromocea <roxanaromocea@gmail.com>
2019-08-06 17:01:44 +0300

    Integrate docker and nginx

--- src/App.js
+++ src/App.js
@@ -23,8 +23,9 @@ class App extends React.Component {
     login = () => {
         const { cookies } = this.props;
         const { username, password } = this.state;
+        const endpoint = process.env.ENDPOINT || '';
 
-        fetch('http://localhost:3000/login', {
+        fetch(`${endpoint}\login`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
